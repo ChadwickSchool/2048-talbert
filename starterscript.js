09b43986ab05d6eb001debf7250fa0cc490da290
tess <tesskoo@gmail.com>
2016-12-13 09:28:12 -0800

    all move functions working, combineTilesUp not working

--- starterscript.js
+++ starterscript.js
@@ -22,18 +22,27 @@ function setUpBoard(){
 		}
 		grid.push(innergrid);
 	}
-	
+
 	addTile();
-	
+
 }
 
 function addTile() {
-	//place a 2 on a random spot in the board	
+	//place a 2 on a random spot in the board
 	var x = Math.round(Math.random()*3);
 	var y = Math.round(Math.random()*3);
+
+	while (grid[x][y] !== "x")
+	{
+		x = Math.round(Math.random()*3);
+		y = Math.round(Math.random()*3);
+	}
+
 	grid[x][y] = "2";
+
+	//adjust to keep running WHILE there's not a 2 at that point
 }
-	
+
 
 function printBoard(){
 	var board = '<br/>' + "*--------------*" + '<br/>';
@@ -46,48 +55,54 @@ function printBoard(){
 		board += "*--------------*";
 		board += '<br/>';
 	}
-	
+
 	//console.log(board);
 	document.getElementById("container").innerHTML = board;
 }
 
 
-//function gets called anytime  a key is pressed 
+//function gets called anytime  a key is pressed
 //e is a special variable
-// that references the event obeject that reads if the user is interacting with 
-//the window 
+// that references the event obeject that reads if the user is interacting with
+//the window
 document.onkeydown = function(e) {
-     
-    //makes it work in internet explorer which uses window.event and not e 
+
+    //makes it work in internet explorer which uses window.event and not e
     e = e || window.event;
 
     //keyCode is actually a character value which we convert to a String
-    //to use triple equals sign 
+    //to use triple equals sign
     if (e.keyCode == UP_ARROW) {
         // up arrow
-        moveTilesUp();
-        
+				moveTilesUp();
+				combineTilesUp();
+
     }
-    //double equals sign will convert it for us 
+    //double equals sign will convert it for us
     else if (e.keyCode == DOWN_ARROW) {
         // down arrow
-        console.log("Pressed down");
+				moveTilesDown();
     }
     else if (e.keyCode == LEFT_ARROW) {
        // left arrow
-       console.log("Pressed left");
+
+			 moveTilesLeft();
     }
     else if (e.keyCode == RIGHT_ARROW) {
        // right arrow
-       console.log("Pressed right");
-    } 
-    
+
+			 moveTilesRight();
+    }
+
+		addTile();
     printBoard(); //have to recall print board to get the board to update
 };
 
 function moveTilesUp()
 {
-    
+
+//work from top down when moving up
+
     for(var r=0; r < grid.length; r++)
     {
         for(var c=0; c<grid[r].length; c++)
@@ -97,9 +112,70 @@ function moveTilesUp()
                 grid[r-1][c] = grid[r][c];
                 grid[r][c] = "x";
             }
-            
+
         }
-        
-    }   
-    
+
+    }
+
+}
+
+function moveTilesDown()
+{
+	for(var r=3; r >= 0; r--)
+	{
+			for(var c=0; c<grid[r].length; c++)
+			{
+					if(r !== 3  && grid[r][c] !== "x" && grid[r+1][c] === "x")
+					{
+							grid[r+1][c] = grid[r][c];
+							grid[r][c] = "x";
+					}
+
+			}
+
+	}
+
+}
+
+function moveTilesRight()
+{
+	for(var c=3; c >= 0; c--)
+	{
+			for(var r=0; r<grid[c].length; r++)
+			{
+					if(c !== 3  && grid[r][c] !== "x" && grid[r][c+1] === "x")
+					{
+							grid[r][c+1] = grid[r][c];
+							grid[r][c] = "x";
+					}
+
+			}
+
+	}
+}
+
+function moveTilesLeft()
+{
+	for(var c=0; c < grid.length; c++)
+	{
+			for(var r=0; r<grid[c].length; r++)
+			{
+					if(c !== 0  && grid[r][c] !== "x" && grid[r][c-1] === "x")
+					{
+							grid[r][c-1] = grid[r][c];
+							grid[r][c] = "x";
+					}
+
+			}
+
+	}
+}
+
+function combineTilesUp()
+{
+	if(r !== 0  && grid[r][c] !== "x" && parseInt(grid[r][c]) == parseInt(grid[r-1][c]))
+	{
+			grid[r-1][c] = grid[r][c];
+			grid[r][c] = parseInt(grid[r][c]) * 2;
+	}
 }
